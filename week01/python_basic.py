091459c513cd7ec47e9a8532f1b457d5e3246cd0
이겨레 <gyeolei66@gmailc.com>
2026-03-12 10:46:55 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -68,3 +68,10 @@ d = "              py           "
 print(d.lstrip())
 print(d.rsplit())
 print(d.strip())
+
+# 문자열 수정 (불가능)
+a = "Python"
+# a[1] = 'y'    # 오류 발생
+
+# 문자열 바꾸기
+a = "Python is difficult"
