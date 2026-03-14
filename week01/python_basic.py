2eb283c7ac9da0abf3d4c39141288f29ae8b8acd
이겨레 <gyeolei66@gmailc.com>
2026-03-14 18:26:58 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -267,4 +267,8 @@ if money >= 1200 and money <=3500:
     print('버스를 탈 수 있습니다.')
 
 # 그룹 자료형의 원소인지 검사하기
-print
+print(1 in [1, 2, 3])
+print(x in [1, 2, 3])
+print(x not in [1, 2, 3])
+print('a' in ['a', 'b', 'c', 'd'])
+print('i' not in 'Python')
