3d24b075e86a10e8ae3306eaf83f19956cdefd82
이겨레 <gyeolei66@gmail.com>
2026-03-20 00:26:40 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -1,3 +1,15 @@
 import pandas as pd
 
 file_url = 'https://media.githubusercontent.com/media/musthave-ML10/data_source/main/sample.csv'
+sample = pd.read.csv(file_url)
+
+print(sample.head())
+print(sample.tail())
+
+sample.info()
+sample.describe()
+
+sample_dic = {'name' : ['John', 'Ann', 'Kevin'], 'age': [23,22,21]}
+a = pd.DataFrame(sample_dic)
+
+a.info()
