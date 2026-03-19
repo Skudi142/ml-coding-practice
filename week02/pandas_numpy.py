61e7853b2e1c0ba7a3c4d8cef10c8c7463e3f88b
이겨레 <gyeolei66@gmail.com>
2026-03-20 01:29:36 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -136,6 +136,10 @@ data = {
     'english': [90, 87, 85, 88, 92]
 }
 
+# Dataframe 생성
+df = pd.DataFrame(data)
+print(df.head())
+
 # 개인별 과목 점수의 평균값 계산 (axis=1)
 df['average'] = df[['math', 'science', 'english']].mean(axis=1)
 print(df)
