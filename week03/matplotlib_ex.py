5d016b525c000c56a458b8a68f60c2456c3a43af
이겨레 <gyeolei66@gmail.com>
2026-03-27 15:04:02 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -397,3 +397,10 @@ plt.suptitle('Survival Analysis by Number of Parents/Children (Parch) on the Tit
 plt.tight_layout()
 plt.savefig('Figure16.png')
 plt.close()
+
+## **그래프 저장하기**
+
+# 히스토그램 그리기
+plt.hist(titanic['Age'], bins=20, color='skyblue', edgecolor='black')
+plt.xlabel('Age')
+plt.ylabel('Count')
