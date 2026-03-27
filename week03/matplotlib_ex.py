2a14ff741abf5bce39384187dbdddb1049769a9c
이겨레 <gyeolei66@gmail.com>
2026-03-27 14:37:01 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -260,3 +260,21 @@ plt.savefig('Figure11.png')
 plt.close()
 
 ## **개별 서브플롯을 하나씩 생성하기**
+plt.subplot(2, 2, 1)
+plt.plot([1, 2, 3])
+
+plt.subplot(2, 2, 2)
+plt.plot([4, 5, 6])
+
+plt.subplot(2, 2, 3)
+plt.plot([7, 8, 9])
+
+plt.subplot(2, 2, 4)
+plt.plot([10, 11, 12])
+plt.savefig('Figure12.png')
+plt.close()
+
+## **타이타닉 데이터셋으로 개별 서브플롯 하나씩 그리기**
+
+# Survived가 0이면 사망자를, 1이면 생존자를 나타냄
+titanic = pd.read_csv('3.1.1.titanic.csv')
