f410a46b622c5e8220b6551f461a2548f6b23ced
이겨레 <gyeolei66@gmail.com>
2026-03-27 13:49:31 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -98,3 +98,5 @@ plt.legend(handles=scatter.legend_elements()[0], title='Survived',
            labels=['Not Survived', 'Survived'], ;oc='upper right')
 plt.savefig('Figure04.png')
 plt.close()
+
+"""### **파이 차트 : 생존자, 사망자 비율 표현하기**"""
