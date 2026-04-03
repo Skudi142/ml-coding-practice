b9e1564c732b5be2ad0a9b6b059ca5fca2c07d3d
이겨레 <gyeolei66@gmail.com>
2026-04-03 17:25:29 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -10,3 +10,14 @@ print(tips.head())
 # ** 범주형 변수 산점도 그래프**
 
 import matplotlib.pyplot as plt
+
+# figure에 2개의 서브 플롯을 생성
+fig = plt.figure(figsize=(15, 5))
+ax1 = fig.add_subplot(1, 2, 1)
+ax2 = fig.add_subplot(1, 2, 2)
+
+# stripplot( ) 그리기
+sns.stripplot(x='day', y='tip', hue='sex', data=tips, alpha=0.7, ax=ax1)
+
+# swarmplot( ) 그리기
+snssns.stripplot(x='day', y='tip', hue='sex', data=tips, alpha=0.7, ax=ax1)
