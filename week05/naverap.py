d8c5d1d79394d90e25e9e96cc23f8cd0a6c5dc5a
이겨레 <gyeolei66@gmail.com>
2026-04-11 16:24:07 +0900

    Updated Changes

--- week05/naverap.py
+++ week05/naverap.py
@@ -37,17 +37,17 @@ def main():
         print('%s_naver_%s.json SAVED' % (srcText, node))
 
 def getNaverSearch(node, srcText, page_start, display):
-        base = "https://openapi.naver.com/v1/search"
-        node = "/%s.json" % node
-        parameters = "?query=%s&start=%s&display=%s" % (urllib.parse.quote(srcText), page_start, display)
+    base = "https://openapi.naver.com/v1/search"
+    node = "/%s.json" % node
+    parameters = "?query=%s&start=%s&display=%s" % (urllib.parse.quote(srcText), page_start, display)
 
-        url = base + node + parameters
-        responseDecode = getRequestUrl(url)               #[CODE 1]
+    url = base + node + parameters
+    responseDecode = getRequestUrl(url)               #[CODE 1]
 
-        if (responseDecode == None):
-            return None
-        else:
-            return json.loads(responseDecode)
+if (responseDecode == None):
+        return None
+    else:
+        return json.loads(responseDecode)
         
 
 def getRequestUrl(url):
@@ -59,33 +59,33 @@ def getRequestUrl(url):
     try:
         response = urllib.request.urlopen(req)
         if response.getcode() == 200:
-                print("[%s] Url Request Success" % datetime.datetime.now())
-                return response.read().decode('utf-8')
-        except Exception as e:
-            print(e)
-            print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
-            return None
+            print("[%s] Url Request Success" % datetime.datetime.now())
+            return response.read().decode('utf-8')
+    except Exception as e:
+        print(e)
+        print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
+        return None
     
-    def getPostData(post, jsonResult, cnt):  #[CODE 3]
-        title = post['title']
-        description = post['description']
-        org_link = post['originallink']
-        link = post['link']
+def getPostData(post, jsonResult, cnt):  #[CODE 3]
+    title = post['title']
+    description = post['description']
+    org_link = post['originallink']
+    link = post['link']
 
-        # %a: 짧은 형식의 요일 이름 (예, 'Mon', "Tue', "Wed', ... )
-        # %d: 일 (예, 01, 02, 03, ...)
-        # %b: 짧은 형식의 월 이름  (예, 'Jan', 'Feb', 'Mar', ...)
-        pDate = datetime.datetime.strptime(post['pubDate'], '%a, %d %b %Y %H:%M:%S +0900')
-        pDate = pDate.strftime('%Y-%m-%d %H:%M:%S')
+    # %a: 짧은 형식의 요일 이름 (예, 'Mon', "Tue', "Wed', ... )
+    # %d: 일 (예, 01, 02, 03, ...)
+    # %b: 짧은 형식의 월 이름  (예, 'Jan', 'Feb', 'Mar', ...)
+    pDate = datetime.datetime.strptime(post['pubDate'], '%a, %d %b %Y %H:%M:%S +0900')
+    pDate = pDate.strftime('%Y-%m-%d %H:%M:%S')
 
-        jsonResult.append({
-            'cnt': cnt,
-            'title': title,
-            'description': description,
-            'org_link': org_link,
-            'link': link,
-            'pDate': pDate
-        })
-        return
+    jsonResult.append({
+        'cnt': cnt,
+        'title': title,
+        'description': description,
+        'org_link': org_link,
+        'link': link,
+        'pDate': pDate
+    })
+    return
     
 main()
