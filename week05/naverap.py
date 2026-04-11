98bf7827dd8def6f4af9430300e787d3e704c5cb
이겨레 <gyeolei66@gmail.com>
2026-04-11 15:37:36 +0900

    Updated Changes

--- week05/naverap.py
+++ week05/naverap.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+# -*- coding: utf-8 -*-
+import urllib.request
+import datetime
+import json
+
+client_id = "YOUR_CLIENT_ID"  # 네이버 개발자 센터에서 발급받은 Client ID
+client_secret = "YOUR_CLIENT_SECRET"  # 네이버 개발자 센터에서 발급받은 Client Secret
+
+def main():
+    
