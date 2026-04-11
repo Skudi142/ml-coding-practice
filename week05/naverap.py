fda4466bdc49fb1792cbff6bfec1e9483bd92971
이겨레 <gyeolei66@gmail.com>
2026-04-11 16:15:37 +0900

    Updated Changes

--- week05/naverap.py
+++ week05/naverap.py
@@ -58,7 +58,7 @@ def main():
         try:
             response = urllib.request.urlopen(req)
             if response.getcode() == 200:
-                print("[%s] Url Request Success" % (datetime.datetime.now(), url))
+                print("[%s] Url Request Success" % datetime.datetime.now())
                 return response.read().decode('utf-8')
         except Exception as e:
             print(e)
