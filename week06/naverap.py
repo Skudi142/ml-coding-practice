a1f199a96284c058323d25b1b5a0003ddb038317
이겨레 <gyeolei66@gmail.com>
2026-04-20 20:04:02 +0900

    Updated Changes

--- week06/naverap.py
+++ week06/naverap.py
@@ -5,3 +5,11 @@ import json
 
 client_id = "w4mKgei7nXxK78IxPOY6"
 client_secret = "8J3bOoYQgu"
+
+def main() :
+
+    node = 'news'                                             # 크롤링할 대상
+    srcText = input('검색어를 입력하세요: ')
+
+    cnt = 0
+    jsonResult = []
