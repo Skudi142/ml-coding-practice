88c58a11f1c4d6acd0ecee55892b3aa46b001b62
이겨레 <gyeolei66@gmail.com>
2026-04-20 20:29:02 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -63,3 +63,12 @@ def getTourismStatsItem(yyyymm, nat_cd, ed_cd):
   parameters += "&YM=" + yyyymm
   parameters += "&NAT_CD=" + nat_cd
   parameters += "&ED_CD=" + ed_cd
+
+   url = service_url + parameters
+
+   responseDecode = getRequestUrl(url) 	                    #[CODE 1]
+
+   if (responseDecode == None):
+     return None
+   else:
+     return json.loads(responseDecode)
