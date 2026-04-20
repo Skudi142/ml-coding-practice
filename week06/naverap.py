2568fc23c24201829bebbed5a60bc031d4af6dbb
이겨레 <gyeolei66@gmail.com>
2026-04-20 20:15:02 +0900

    Updated Changes

--- week06/naverap.py
+++ week06/naverap.py
@@ -36,7 +36,7 @@ def main() :
     print('가져온 데이터 : %d 건' % cnt)
     print('%s_naver_%s.json SAVED' % (srcText, node))
 
-    def getNaverSearch(node, srcText, page_start, display) :
+   def getNaverSearch(node, srcText, page_start, display):
     base = "https://openapi.naver.com/v1/search"
     node = "/%s.json" % node
     parameters = "?query=%s&start=%s&display=%s" % (urllib.parse.quote(srcText), page_start, display)
