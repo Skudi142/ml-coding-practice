f40ec78b0ff1f42a924f35a374aa2d0ed87d31a9
이겨레 <gyeolei66@gmail.com>
2026-04-20 20:20:32 +0900

    Updated Changes

---
+++ week06/tourism.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import urllib.request
+import datetime
+import json
+import pandas as pd
+
+ServiceKey = "264d5f2c363e6ede17ff1f274e9dacf9eee7e40fdbbe2c344678b547d7b1db70"
+
+"""### [CODE 0]"""
