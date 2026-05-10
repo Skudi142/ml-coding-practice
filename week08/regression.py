31ce5b5161f6b43813b50da0c0358cadf08d8d39
이겨레 <gyeolei66@gmail.com>
2026-05-10 17:49:39 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -7,3 +7,10 @@ plt.rc('legend', fontsize=14)
 plt.rc('xtick', labelsize=10)
 plt.rc('ytick', labelsize=10)
 
+import numpy as np
+
+np.random.seed(42)
+
+m = 100
+X = 2 * np.random.rand(m, 1)
+y = 4 + 3 * X + np.random.randn(m, 1)
