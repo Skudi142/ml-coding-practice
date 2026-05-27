f1056b21047772a7633dd21fe56ed04d1f27f087
이겨레 <gyeolei66@gmail.com>
2026-05-27 16:38:50 +0900

    Updated Changes

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -27,3 +27,14 @@ Source.from_file("iris_tree.dot")
 
 print(tree_clf.predict_proba([[5, 1.5]]).round(3))
 print(tree_clf.predict([[5, 1.5]]))
+
+# 규제 매개변수
+
+from sklearn.datasets import make_moons
+
+X_moons, y_moons = make_moons(n_samples=150, noise=0.2, random_state=42)
+
+tree_clf1 = DecisionTreeClassifier(random_state=42)
+tree_clf2 = DecisionTreeClassifier(min_samples_leaf=5, random_state=42)
+tree_clf1.fit(X_moons, y_moons)
+tree_clf2.fit(X_moons, y_moons)
